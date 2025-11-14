7f71d0481b10c2fcb1838540526175925180e1ae
onebeastchris <github@onechris.mozmail.com>
2025-11-14 16:44:20 +0100

    Yeet config since `show-emotes` is now a Geyser config option

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "9.2.2"
 }
 
 group = "org.geysermc.extension.emoteoffhand"
@@ -15,31 +12,18 @@ repositories {
 
 dependencies {
     compileOnly("org.geysermc.geyser:api:2.9.0-SNAPSHOT")
-
-    implementation("org.yaml:snakeyaml:2.5")
-    shadow("org.yaml:snakeyaml:2.5")
 }
 
 tasks {
     named<Jar>("jar") {
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        archiveFileName.set("${project.name}-${project.version}-nonrelocated.jar")
+        archiveFileName.set("${project.name}.jar")
 
         from({
             configurations.runtimeClasspath.get().map { file ->
                 if (file.isDirectory) file else zipTree(file)
             }
         }, project.rootProject.file("LICENSE"))
-    }
-
-    val shadowJar = named<ShadowJar>("shadowJar") {
-        archiveFileName.set("${project.name}.jar")
-        from(project.rootProject.file("LICENSE"))
-        relocate("org.yaml.snakeyaml", "org.geysermc.extension.emoteoffhand.shadow.snakeyaml")
-    }
-
-    named("build") {
-        dependsOn(shadowJar)
     }
 }
 
